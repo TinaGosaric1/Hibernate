f71fdb5c47e8a02a0741eeb56cd69269ad8c31e3
TinaADM <TinaADM@nb230870.ad.gorenje.si>
2023-02-03 09:52:14 +0100

    Retrieving an object from the database

--- src/com/tg/Main.java
+++ src/com/tg/Main.java
@@ -25,9 +25,29 @@ public class Main {
 
             // save the student object
             System.out.println("Saving the student...");
+            System.out.println(tempStudent);
             session.save(tempStudent);
 
             // commit transaction
+            session.getTransaction().commit();
+
+            // MY NEW CODE
+
+            // find out the student's id: primary key
+            System.out.println("Saved student. Generated id: " + tempStudent.getId());
+
+            // now get a new session and start transaction
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            // retrieve student based on the id: primary key
+            System.out.println("\nGetting student with id: " + tempStudent.getId());
+
+            Student myStudent = session.get(Student.class, tempStudent.getId());
+
+            System.out.println("Get complete: " + myStudent);
+
+            // commit the transaction
             session.getTransaction().commit();
 
             System.out.println("Done!");
